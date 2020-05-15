e8c709fc55c65f6d866b95096a97e4b617e86124
Bryan Garza <lagarzab@gmail.com>
2020-05-15 17:07:32 -0400

    Added P5 w/ minimum scripts for POC

--- components/canvas.vue
+++ components/canvas.vue
@@ -1,8 +1,12 @@
 <template>
-    <div :id="elementId">Element ID added</div>
+    <div class='canvas-parent'>
+        <div :id='elementId' class='canvas-parent__canvas'></div>
+    </div>
 </template>
 
 <script>
+import P5 from 'p5'
+
 export default {
     name: 'p5Canvas',
     props: {
@@ -11,8 +15,37 @@ export default {
             required: true
         }
     },
+    created () {
+        const p5Script = (p5) => {
+
+            // P5 Setup
+            p5.setup = () => {
+                let w = window.innerWidth - 20
+                let h = window.innerHeight - 20
+                p5.createCanvas(w, h)
+                  .parent(this.elementId)
+            }
+
+            // Loop, which continues to draw canvas
+            p5.draw = () => {
+                p5.background([ 255, 0, 0 ])
+            }
+        }
+
+        /* P5 instance mode else we may experience issues
+           with P5 being created in global scope */
+        new P5(p5Script)
+    },
 }
 </script>
 
 <style lang='scss'>
+.canvas-parent {
+    position: relative;
+    &__canvas {
+        position:absolute;
+        left:10px;
+        top:10px;
+    }
+}
 </style>
